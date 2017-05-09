780f5531f18ee714a4d28710d18cb13ce3ba0530
Nishant Patel <contact@nishant-patel.com>
2017-05-09 03:14:53 -0700

    Testing

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -14,7 +14,7 @@ class MySQLConnection(object):
         }
         # this will use the above values to generate the path to connect to your sql database
         DATABASE_URI = "mysql://{}:{}@127.0.0.1:{}/{}".format(config['user'], config['password'], config['port'], config['database'])
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLEARDB_DATABASE_URL'] || DATABASE_URI
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLEARDB_DATABASE_URL']
         print os.environ['CLEARDB_DATABASE_URL']
         print app.config['SQLALCHEMY_DATABASE_URI']
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
