054da57be25b3c0ec061ba7e6a048c022ab3ff4d
Nishant Patel <contact@nishant-patel.com>
2017-05-09 03:07:46 -0700

    test

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -13,7 +13,7 @@ class MySQLConnection(object):
         }
         # this will use the above values to generate the path to connect to your sql database
         DATABASE_URI = "mysql://{}:{}@127.0.0.1:{}/{}".format(config['user'], config['password'], config['port'], config['database'])
-        app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLEARDB_DATABASE_URL']
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
         # establish the connection to database
         self.db = SQLAlchemy(app)
